74e068b1c664f4436eee7365152d273414dbcdb0
VegeBun-csj <csj1071285741@gmail.com>
2022-09-04 22:01:57 +0800

    dir structure

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://github.com/DoraFactory/multisig-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Multisig app front-end
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
