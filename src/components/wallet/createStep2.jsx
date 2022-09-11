f51638ad0924f0f56ff2d6d790557df82203c96e
99kies <1290017556@qq.com>
2022-09-11 14:40:36 +0800

    update number input

--- src/components/wallet/createStep2.jsx
+++ src/components/wallet/createStep2.jsx
@@ -57,6 +57,17 @@ const CreateStep2 = () => {
         console.log(multisigAccount)
     }
 
+    function clearNoNum(event) {
+        event.target.value = event.target.value.replace(/[^\d.]/g, "");  //清除“数字”和“.”以外的字符
+        event.target.value = event.target.value.replace(/\.{2,}/g, "."); //只保留第一个. 清除多余的
+        event.target.value = event.target.value.replace(".", "$#$").replace(/\./g, "").replace("$#$", ".");
+        event.target.value = event.target.value.replace(/^(\-)*(\d+)\.(\d\d).*$/, '$1$2.$3');//只能输入两个小数
+        if (event.target.value.indexOf(".") < 0 && event.target.value != "") {//以上已经过滤，此处控制的是如果没有小数点，首位不能为类似于 01、02的金额
+            event.target.value = parseFloat(event.target.value);
+        }
+    }
+
+
     const handleInputChange = (event) => {
         const value = event.target.value;
         const inputType = event.target.id.split(' ')[0];
@@ -150,7 +161,7 @@ const CreateStep2 = () => {
             <section>
                 3. Any transaction requires the confirmation of:
                 <div className="threshold">
-                    <input type="number"  ref={threshold}/>
+                <input type="number" οnkeypress="value=value.replace('-','')" min="0" ref={threshold}/>
                     <span>out of owner(s)</span>
                 </div>
             </section>
