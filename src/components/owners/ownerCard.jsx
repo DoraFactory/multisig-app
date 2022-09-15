dd3f50978e7f2bd5ef7dc4737b5cbf82ef83b67e
99kies <1290017556@qq.com>
2022-09-15 17:46:35 +0800

    update ownercard

--- src/components/owners/ownerCard.jsx
+++ src/components/owners/ownerCard.jsx
@@ -2,11 +2,18 @@
 import '../../styles/owners.scss'
 import avatar from '../../resources/avatar.svg'
 import copy from '../../resources/copy.svg'
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 
 function OwnerCard () {
 
     const owners = JSON.parse(localStorage.getItem('multisig-wallet')).owners;
+    const showAccount = (str, maxlength = 7) => {
+      const length = str.length;
+      return str.length > maxlength
+        ? str.slice(0, maxlength - 1) + '...' + str.slice(46, length)
+        : str;
+    };
 
     return (
       <div class="all-owners">
@@ -15,12 +22,20 @@ function OwnerCard () {
           {owners.map((owner) => (
             <div class="owner-card">
               <img class="owner-logo" src={avatar}/>
-              <p> {owner.name} </p>
-              <div>
+              <p class="owner-name"> {owner.name} </p>
+              <div class="flex-container">
+                {/* <div > */}
                 <p class="address">
-                  {owner.account}
+                  {showAccount(owner.account, 7)}
                 </p>
-                <img src={copy}/>
+                {/* </div> */}
+                {/* <div> */}
+                  <CopyToClipboard
+                      text={owner.account}
+                  >
+                    <img src={copy}/>
+                  </CopyToClipboard>
+                {/* </div> */}
               </div>
             </div>
           ))}
