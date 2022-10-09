4506c545f406b9a9fb6bcdce48c501462778faa1
99kies <1290017556@qq.com>
2022-10-09 19:26:21 +0800

    update url

--- src/components/login/login_choose_user.jsx
+++ src/components/login/login_choose_user.jsx
@@ -121,7 +121,7 @@ const LoginUserCard = () => {
 
             if(result.data['token']){
                 sessionStorage.setItem("token", result.data['token'].toString())
-                const wallets = await axios.get(`http://127.0.0.1:8000/wallets/${currentAccount.address}/`,{headers: {"dorafactory-token": sessionStorage.getItem("token")}})
+                const wallets = await axios.get(`http://127.0.0.1:8000/wallets/${currentAccount.address.toString()}/`,{headers: {"dorafactory-token": sessionStorage.getItem("token")}})
                     .then((res) => {
                         return res.data
                     });
